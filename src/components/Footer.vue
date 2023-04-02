9ef8a03d99dbe8295a5459492d3f035033efecc9
Sergei Česnakas <10352420+cesnakas@users.noreply.github.com>
2023-04-03 02:47:47 +0400

    Update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer role="contentinfo">
     <div class="container py-5">
       <div class="small">
-        <small class="text-muted">Copyright © 2023 Česnakas Inc. All rights reserved.</small>
+        <small class="text-muted">Copyright © 2023 Česnakas Inc. All rights reserved. </small>
       </div>
     </div>
   </footer>
