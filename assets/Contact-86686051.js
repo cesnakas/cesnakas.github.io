a4fc7bab11bc165bc0df049447702508bee6544e
Sergei Česnakas <10352420+cesnakas@users.noreply.github.com>
2023-04-03 12:40:55 +0400

    deploy

---
+++ assets/Contact-86686051.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-47c6975f.js";const c={};function n(r,e){return null}const o=t(c,[["render",n]]);export{o as default};
--- assets/Resume-36a25062.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-a6f6a231.js";const r={};function c(n,t){return null}const s=e(r,[["render",c]]);export{s as default};
